42fc4a23b5165e96fdf3989732a29f9c05a4e28d
Benedict <benedictiknkeonye@gmail.com>
2021-12-11 14:36:12 +0100

    Feat: wired up base components

--- src/app/Base/base-button/base-button.component.ts
+++ src/app/Base/base-button/base-button.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, Output, EventEmitter, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-base-button',
   templateUrl: './base-button.component.html',
-  styleUrls: ['./base-button.component.css']
+  styleUrls: ['./base-button.component.css'],
 })
 export class BaseButtonComponent implements OnInit {
+  @Input() type?: string;
+  @Input() mode?: string;
+  @Output() onClick = new EventEmitter<any>();
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  onClickBtn(event: any) {
+    this.onClick.emit(event);
   }
-
 }
